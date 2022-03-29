d8868533dfaf3d42e919e28bc240ece35b43c020
[[Karl Ulm]] <[[karl.ulm@relativity.com]]>
2022-03-29 16:32:51 -0500

    my first commmit

---
+++ SpecFlowProject1/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+        private int _firstNumber;
+        private int _secondNumber;
+        private int _result;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _firstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _secondNumber = number; 
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _firstNumber + _secondNumber;
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            Assert.AreEqual(result, _result);
+        }
+
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            _result = _firstNumber - _secondNumber;
+        }
+    }
+}
